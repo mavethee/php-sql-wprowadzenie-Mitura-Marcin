c6b944336440f2d290c9851db03b25a8bfc1a7be
Marcin Mitura <mitura07@wp.pl>
2020-10-29 11:22:34 +0100

    Uploading index.php with full name

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width-device-wdith, initial scale=1.0" charset="UTF-8">
+<title>Document</title>
+</head>
+<body>
+<div class="a"></div>
+<?php
+//Podpisanie pracy
+echo("<h1>Marcin Mitura</h1>");
+echo("<h2>Klasa 2Ti, grupa 1</h2>");
+echo("<h3>Numer 22</h3>");
+?>
+</body>
+</html>
