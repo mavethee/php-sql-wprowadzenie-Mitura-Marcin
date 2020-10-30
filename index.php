9df99a74c4c84f1aacf71bf32d8c67cdceb35e13
Marcin Mitura <mitura07@wp.pl>
2020-10-30 14:55:09 +0100

    Updating typo with `id` instead `id_pracownicy`

--- index.php
+++ index.php
@@ -21,14 +21,14 @@ echo("<h1>Zadanie 1: Wyświetlenie pełnej listy pracowników:</h1>");
 $conn=new mysqli("mysql-mavethee.alwaysdata.net", "mavethee_marcin", "H@slo24!", "mavethee_php-mitura-marcin");
 $result=$conn->query("SELECT * FROM pracownicy");
 echo("<table borded=1>");
-echo("<th>id</th>");
+echo("<th>id_pracownicy</th>");
 echo("<th>imie</th>");
 echo("<th>dzial</th>");
 echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
@@ -38,14 +38,14 @@ echo("<h1>Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:</h1>");
 $conn=new mysqli("mysql-mavethee.alwaysdata.net", "mavethee_marcin", "H@slo24!", "mavethee_php-mitura-marcin");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a'");
 echo("<table borded=1>");
-echo("<th>id</th>");
+echo("<th>id_pracownicy</th>");
 echo("<th>imie</th>");
 echo("<th>dzial</th>");
 echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
@@ -55,14 +55,14 @@ echo("<h1>Zadanie 3: Kobiety z działu 1 i 4:</h1>");
 $conn=new mysqli("mysql-mavethee.alwaysdata.net", "mavethee_marcin", "H@slo24!", "mavethee_php-mitura-marcin");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND dzial=1 OR dzial=4");
 echo("<table borded=1>");
-echo("<th>id</th>");
+echo("<th>id_pracownicy</th>");
 echo("<th>imie</th>");
 echo("<th>dzial</th>");
 echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
@@ -72,14 +72,14 @@ echo("<h1>Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:</h1>");
 $conn=new mysqli("mysql-mavethee.alwaysdata.net", "mavethee_marcin", "H@slo24!", "mavethee_php-mitura-marcin");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND (zarobki<35) AND (dzial=1 OR dzial=2)");
 echo("<table borded=1>");
-echo("<th>id</th>");
+echo("<th>id_pracownicy</th>");
 echo("<th>imie</th>");
 echo("<th>dzial</th>");
 echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
@@ -89,14 +89,14 @@ echo("<h1>Zadanie 5: Mężczyźni zarabiający mniej niż 30:</h1>");
 $conn=new mysqli("mysql-mavethee.alwaysdata.net", "mavethee_marcin", "H@slo24!", "mavethee_php-mitura-marcin");
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie NOT LIKE '%a' AND (zarobki<30)");
 echo("<table borded=1>");
-echo("<th>id</th>");
+echo("<th>id_pracownicy</th>");
 echo("<th>imie</th>");
 echo("<th>dzial</th>");
 echo("<th>zarobki</th>");
 while($row=$result->fetch_assoc())
 {
 echo("<tr>");
-echo("<td>".$row["id"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
