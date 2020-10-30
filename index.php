c7339d52c648cfe9e01748b846d1d64324052711
Marcin Mitura <mitura07@wp.pl>
2020-10-30 15:34:07 +0100

    Removing not needed $conn lines

--- index.php
+++ index.php
@@ -11,12 +11,10 @@ echo("<h2>Klasa 2Ti, grupa 1</h2>");
 echo("<h3>Numer 22</h3>");
 
 //Logowanie do serwera mySQL
-echo(phpinfo());
 require_once("connect.php");
 
 //Zadanie 1: Wyświetlenie pełnej listy pracowników:
 echo("<h1>Zadanie 1: Wyświetlenie pełnej listy pracowników:</h1>");
-$conn=new mysqli($servername, $username, $password, $dbname);
 $result=$conn->query("SELECT * FROM pracownicy");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -33,7 +31,6 @@ echo("</table>");
 
 //Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:
 echo("<h1>Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:</h1>");
-$conn=new mysqli($servername, $username, $password, $dbname);
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a'");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -50,7 +47,6 @@ echo("</table>");
 
 //Zadanie 3: Kobiety z działu 1 i 4:
 echo("<h1>Zadanie 3: Kobiety z działu 1 i 4:</h1>");
-$conn=new mysqli($servername, $username, $password, $dbname);
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND dzial=1 OR dzial=4");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -67,7 +63,6 @@ echo("</table>");
 
 //Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:
 echo("<h1>Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:</h1>");
-$conn=new mysqli($servername, $username, $password, $dbname);
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND (zarobki<35) AND (dzial=1 OR dzial=2)");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -84,7 +79,6 @@ echo("</table>");
 
 //Zadanie 5: Mężczyźni zarabiający mniej niż 30:
 echo("<h1>Zadanie 5: Mężczyźni zarabiający mniej niż 30:</h1>");
-$conn=new mysqli($servername, $username, $password, $dbname);
 $result=$conn->query("SELECT * FROM pracownicy WHERE imie NOT LIKE '%a' AND (zarobki<30)");
 echo("<table borded=1>");
 echo("<th>id</th>");
@@ -101,7 +95,6 @@ echo("</table>");
 
 //Zadanie 6: Wyświetlenie "MOJA_SUMA" (funkcja SUM)
 echo("<h1>Zadanie 6: Wyświetlenie 'MOJA_SUMA' (funkcja SUM)</h1>");
-$conn=new mysqli($servername, $username, $password, $dbname);
 $result=$conn->query("SELECT dzial, SUM(zarobki) AS MOJA_SUMA FROM pracownicy GROUP BY dzial");
 echo("<table borded=1>");
 echo("<th>dzial</th>");
@@ -113,6 +106,9 @@ echo("<td>".$row["dzial"]."</td><td>".$row["MOJA_SUMA"]."</td>");
 echo("</tr>");
 }
 echo("</table>");
+
+//phpinfo na koniec pliku
+echo(phpinfo());
 ?>
 </body>
 </html>
