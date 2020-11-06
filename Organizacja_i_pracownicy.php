2760acf1ce974f7afdc8f4015bf204a8b9bf7cd7
Marcin Mitura <mitura07@wp.pl>
2020-11-06 19:10:34 +0100

    Adding some additional SQL lines
    ShookChamp

--- Organizacja_i_pracownicy.php
+++ Organizacja_i_pracownicy.php
@@ -5,12 +5,19 @@
         <link rel="stylesheet" href="style.css">
     </head>
 <body>
-    <?php
+    <div class="nav">
+            Place for navigation and links
+            <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy<a>;
+            <a href="Funkcje_agregujace.php">Funkcje_agregujące<a>;
+            <a href="index.php">Główny index<a>;
+            <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub<a>;
+    </div>
+        <?php
 
-    //Logowanie do serwera mySQL
-    require_once("connect.php");
+        //Logowanie do serwera mySQL
+        require_once("connect.php");
 
-    echo("Organizacja_i_pracownicy.php");
+        echo("Organizacja_i_pracownicy.php");
         //Zadanie 1: Wyświetlenie pełnej listy pracowników z nazwami działów
         echo("<h1>Zadanie 1: Wyświetlenie pełnej listy pracowników z nazwami działów</h1>");
         $result=$conn->query("SELECT imie, dzial, nazwa_dzial FROM pracownicy, organizacja WHERE dzial=id_org");
@@ -70,6 +77,21 @@
             echo("</tr>");
         }
         echo("</table>");
-    ?>
+
+        //Zadanie 5: Wyświetlenie pracowników tylko z działu 2 i 3:
+        echo("<h1>Zadanie 5: Wyświetlenie pracowników tylko z działu 2 i 3</h1>");
+        $result=$conn->query("SELECT imie, dzial, nazwa_dzial FROM pracownicy, organizacja WHERE (dzial=2 OR dzial=3) AND dzial=id_org GROUP BY dzial");
+        echo("<table borded=1>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>nazwa_dzial</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
+        ?>
 </body>
 </html>
