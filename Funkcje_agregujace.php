23bc8bbfcd00d6cb6ce300486065dc8db25ad694
Marcin Mitura <mitura07@wp.pl>
2020-11-07 23:32:45 +0100

    New SQL variables and making tables look like ones

--- Funkcje_agregujace.php
+++ Funkcje_agregujace.php
@@ -5,89 +5,108 @@
         <link rel="stylesheet" href="style.css">
     </head>
 <body>
-    <?php
+    <div class="nav">
+        Place for navigation and links
+        <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy<a>;
+        <a href="index.php">Główny index<a>;
+        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub<a>;
+    </div>
+        <?php
 
-    //Logowanie do serwera mySQL
-    require_once("connect.php");
+        //Logowanie do serwera mySQL
+        require_once("connect.php");
 
-    echo("Funkcje_agregujące.php");
-        //Zadanie 1: Wyświetlenie sumy zarobków wszystkich pracowników
-        echo("<h1>Zadanie 1: Wyświetlenie sumy zarobków wszystkich pracowników</h1>");
-        $result=$conn->query("SELECT SUM(zarobki) AS Suma_zarobków_wszystkich_pracowników FROM pracownicy");
-        echo("<table borded=1>");
-        echo("<th>Suma_zarobków_wszystkich_pracowników</th>");
-        while($row=$result->fetch_assoc())
-        {
-            echo("<tr>");
-            echo("<td>".$row["Suma_zarobków_wszystkich_pracowników"]."</td>");
-            echo("</tr>");
-        }
-        echo("</table>");
+        echo("Funkcje_agregujące.php");
+            //Zadanie 1: Wyświetlenie sumy zarobków wszystkich pracowników
+            echo("<h1>Zadanie 1: Wyświetlenie sumy zarobków wszystkich pracowników</h1>");
+            $result=$conn->query("SELECT SUM(zarobki) AS Suma_zarobków_wszystkich_pracowników FROM pracownicy");
+            echo("<table borded=1>");
+            echo("<th>Suma_zarobków_wszystkich_pracowników</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["Suma_zarobków_wszystkich_pracowników"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
 
-        //Zadanie 2: Wyświetlenie pracowników tylko z działu 1 i 4:
-        echo("<h1>Zadanie 2: Wyświetlenie pracowników tylko z działu 1 i 4</h1>");
-        $result=$conn->query("SELECT SUM(zarobki) AS Suma_zarobków_kobiet FROM pracownicy WHERE imie NOT LIKE '%a'");
-        echo("<table borded=1>");
-        echo("<th>Suma_zarobków_kobiet</th>");
-        while($row=$result->fetch_assoc())
-        {
-            echo("<tr>");
-            echo("<td>".$row["Suma_zarobków_kobiet"]."</td>");
-            echo("</tr>");
-        }
-        echo("</table>");
+            //Zadanie 2: Wyświetlenie pracowników tylko z działu 1 i 4:
+            echo("<h1>Zadanie 2: Wyświetlenie pracowników tylko z działu 1 i 4</h1>");
+            $result=$conn->query("SELECT SUM(zarobki) AS Suma_zarobków_kobiet FROM pracownicy WHERE imie NOT LIKE '%a'");
+            echo("<table borded=1>");
+            echo("<th>Suma_zarobków_kobiet</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["Suma_zarobków_kobiet"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
 
-        //Zadanie 3: Wyświetlenie sumy zarobków mężczyzn pracujących w dziale 2 i 3:
-        echo("<h1>Zadanie 3: Wyświetlenie sumy zarobków mężczyzn pracujących w dziale 2 i 3</h1>");
-        $result=$conn->query("SELECT SUM(zarobki) AS Suma_zarobków_mężczyzn_pracujących_w_dziale_2_i_3 FROM pracownicy WHERE imie NOT LIKE '%a' and (dzial=2 OR dzial=3)");
-        echo("<table borded=1>");
-        echo("<th>Suma_zarobków_mężczyzn_pracujących_w_dziale_2_i_3</th>");
-        while($row=$result->fetch_assoc())
-        {
-            echo("<tr>");
-            echo("<td>".$row["Suma_zarobków_mężczyzn_pracujących_w_dziale_2_i_3"]."</td>");
-            echo("</tr>");
-        }
-        echo("</table>");
+            //Zadanie 3: Wyświetlenie sumy zarobków mężczyzn pracujących w dziale 2 i 3:
+            echo("<h1>Zadanie 3: Wyświetlenie sumy zarobków mężczyzn pracujących w dziale 2 i 3</h1>");
+            $result=$conn->query("SELECT SUM(zarobki) AS Suma_zarobków_mężczyzn_pracujących_w_dziale_2_i_3 FROM pracownicy WHERE imie NOT LIKE '%a' and (dzial=2 OR dzial=3)");
+            echo("<table borded=1>");
+            echo("<th>Suma_zarobków_mężczyzn_pracujących_w_dziale_2_i_3</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["Suma_zarobków_mężczyzn_pracujących_w_dziale_2_i_3"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
 
-        //Zadanie 4: Średnia zarobków pracowników z działu 4:
-        echo("<h1>Zadanie 4: Średnia zarobków pracowników z działu 4</h1>");
-        $result=$conn->query("SELECT AVG(zarobki) AS Średnia_zarobków_pracowników_z_działu_4 FROM pracownicy WHERE dzial=4");
-        echo("<table borded=1>");
-        echo("<th>Średnia_zarobków_pracowników_z_działu_4</th>");
-        while($row=$result->fetch_assoc())
-        {
-            echo("<tr>");
-            echo("<td>".$row["Średnia_zarobków_pracowników_z_działu_4"]."</td>");
-            echo("</tr>");
-        }
-        echo("</table>");
+            //Zadanie 4: Średnia zarobków pracowników z działu 4:
+            echo("<h1>Zadanie 4: Średnia zarobków pracowników z działu 4</h1>");
+            $result=$conn->query("SELECT AVG(zarobki) AS Średnia_zarobków_pracowników_z_działu_4 FROM pracownicy WHERE dzial=4");
+            echo("<table borded=1>");
+            echo("<th>Średnia_zarobków_pracowników_z_działu_4</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["Średnia_zarobków_pracowników_z_działu_4"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
 
-        //Zadanie 5: Średnia zarobków mężczyzn z działu 1 i 2:
-        echo("<h1>Zadanie 5: Średnia zarobków mężczyzn z działu 1 i 2</h1>");
-        $result=$conn->query("SELECT AVG(zarobki) AS Średnia_zarobków_mężczyzn_z_działu_1_i_2 FROM pracownicy WHERE dzial=4");
-        echo("<table borded=1>");
-        echo("<th>Średnia_zarobków_mężczyzn_z_działu_1_i_2</th>");
-        while($row=$result->fetch_assoc())
-        {
-            echo("<tr>");
-            echo("<td>".$row["Średnia_zarobków_pracowników_z_działu_1_i_2"]."</td>");
-            echo("</tr>");
-        }
-        echo("</table>");
+            //Zadanie 5: Średnia zarobków mężczyzn z działu 1 i 2:
+            echo("<h1>Zadanie 5: Średnia zarobków mężczyzn z działu 1 i 2</h1>");
+            $result=$conn->query("SELECT AVG(zarobki) AS Średnia_zarobków_mężczyzn_z_działu_1_i_2 FROM pracownicy WHERE dzial=4");
+            echo("<table borded=1>");
+            echo("<th>Średnia_zarobków_mężczyzn_z_działu_1_i_2</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["Średnia_zarobków_pracowników_z_działu_1_i_2"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
 
-        //Zadanie 6: Ilość wszystkich pracowników:
-        echo("<h1>Zadanie 6: Ilość wszystkich pracowników</h1>");
-        $result=$conn->query("SELECT COUNT(imie) AS Ilość_wszystkich_pracowników FROM pracownicy");
-        echo("<table borded=1>");
-        echo("<th>Ilość_wszystkich_pracowników</th>");
-        while($row=$result->fetch_assoc())
-        {
-            echo("<tr>");
-            echo("<td>".$row["Ilość_wszystkich_pracowników"]."</td>");
-            echo("</tr>");
-        }
-        echo("</table>");
-    ?>
+            //Zadanie 6: Ilość wszystkich pracowników:
+            echo("<h1>Zadanie 6: Ilość wszystkich pracowników</h1>");
+            $result=$conn->query("SELECT COUNT(imie) AS Ilość_wszystkich_pracowników FROM pracownicy");
+            echo("<table borded=1>");
+            echo("<th>Ilość_wszystkich_pracowników</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["Ilość_wszystkich_pracowników"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
+
+            //Zadanie 7: Ilość kobiet pracujących w dziale 1 i 3:
+            echo("<h1>Zadanie 7: Ilość kobiet pracujących w dziale 1 i 3</h1>");
+            $result=$conn->query("SELECT COUNT(imie) AS Ilość_kobiet_pracujących_w_dziale_1_i_3 FROM pracownicy WHERE imie LIKE '%a'");
+            echo("<table borded=1>");
+            echo("<th>Ilość_kobiet_pracujących_w_dziale_1_i_3</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["Ilość_kobiet_pracujących_w_dziale_1_i_3"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
+        ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,142 +8,158 @@
     <div class="nav">
         Place for navigation and links
         <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy<a>;
-        <a href="Funkcje_agregujace.php">Funkcje_agregujace<a>;
+        <a href="Funkcje_agregujace.php">Funkcje_agregujące<a>;
+        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub<a>;
     </div>
-    <?php
-    //Podpisanie pracy
-    echo("<h1>Marcin Mitura</h1>");
-    echo("<h2>Klasa 2Ti, grupa 1</h2>");
-    echo("<h3>Numer 22</h3>");
+        <?php
+        //Podpisanie pracy
+        echo("<h1>Marcin Mitura</h1>");
+        echo("<h2>Klasa 2Ti, grupa 1</h2>");
+        echo("<h3>Numer 22</h3>");
 
-    //Logowanie do serwera mySQL
-    require_once("connect.php");
+        //Logowanie do serwera mySQL
+        require_once("connect.php");
 
-    //Zadanie 1: Wyświetlenie pełnej listy pracowników:
-    echo("<h1>Zadanie 1: Wyświetlenie pełnej listy pracowników:</h1>");
-    $result=$conn->query("SELECT * FROM pracownicy");
-    echo("<table borded=1>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>dzial</th>");
-    echo("<th>zarobki</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 1: Wyświetlenie pełnej listy pracowników:
+        echo("<h1>Zadanie 1: Wyświetlenie pełnej listy pracowników:</h1>");
+        $result=$conn->query("SELECT * FROM pracownicy");
+        echo("<table borded=1>");
+        echo("<th>id_pracownicy</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
 
-    //Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:
-    echo("<h1>Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:</h1>");
-    $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a'");
-    echo("<table borded=1>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>dzial</th>");
-    echo("<th>zarobki</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:
+        echo("<h1>Zadanie 2: Wyświetlenie tylko kobiet listy pracowników:</h1>");
+        $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a'");
+        echo("<table borded=1>");
+        echo("<th>id_pracownicy</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
 
-    //Zadanie 3: Kobiety z działu 1 i 4:
-    echo("<h1>Zadanie 3: Kobiety z działu 1 i 4:</h1>");
-    $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND dzial=1 OR dzial=4");
-    echo("<table borded=1>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>dzial</th>");
-    echo("<th>zarobki</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 3: Kobiety z działu 1 i 4:
+        echo("<h1>Zadanie 3: Kobiety z działu 1 i 4:</h1>");
+        $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND dzial=1 OR dzial=4");
+        echo("<table borded=1>");
+        echo("<th>id_pracownicy</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
 
-    //Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:
-    echo("<h1>Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:</h1>");
-    $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND (zarobki<35) AND (dzial=1 OR dzial=2)");
-    echo("<table borded=1>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>dzial</th>");
-    echo("<th>zarobki</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:
+        echo("<h1>Zadanie 4: Kobiety zarabiające mniej niż 35 z działu 1 i 2:</h1>");
+        $result=$conn->query("SELECT * FROM pracownicy WHERE imie LIKE '%a' AND (zarobki<35) AND (dzial=1 OR dzial=2)");
+        echo("<table borded=1>");
+        echo("<th>id_pracownicy</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
 
-    //Zadanie 5: Mężczyźni zarabiający mniej niż 30:
-    echo("<h1>Zadanie 5: Mężczyźni zarabiający mniej niż 30:</h1>");
-    $result=$conn->query("SELECT * FROM pracownicy WHERE imie NOT LIKE '%a' AND (zarobki<30)");
-    echo("<table borded=1>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>dzial</th>");
-    echo("<th>zarobki</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 5: Mężczyźni zarabiający mniej niż 30:
+        echo("<h1>Zadanie 5: Mężczyźni zarabiający mniej niż 30:</h1>");
+        $result=$conn->query("SELECT * FROM pracownicy WHERE imie NOT LIKE '%a' AND (zarobki<30)");
+        echo("<table borded=1>");
+        echo("<th>id_pracownicy</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
 
-    //Zadanie 6: Wyświetlenie "MOJA_SUMA" (funkcja SUM)
-    echo("<h1>Zadanie 6: Wyświetlenie 'MOJA_SUMA' (funkcja SUM)</h1>");
-    $result=$conn->query("SELECT dzial, SUM(zarobki) AS MOJA_SUMA FROM pracownicy GROUP BY dzial");
-    echo("<table borded=1>");
-    echo("<th>dzial</th>");
-    echo("<th>MOJA_SUMA</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["dzial"]."</td><td>".$row["MOJA_SUMA"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 6: Wyświetlenie "MOJA_SUMA" (funkcja SUM)
+        echo("<h1>Zadanie 6: Wyświetlenie 'MOJA_SUMA' (funkcja SUM)</h1>");
+        $result=$conn->query("SELECT dzial, SUM(zarobki) AS MOJA_SUMA FROM pracownicy GROUP BY dzial");
+        echo("<table borded=1>");
+        echo("<th>dzial</th>");
+        echo("<th>MOJA_SUMA</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["dzial"]."</td><td>".$row["MOJA_SUMA"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
 
-    //Zadanie 7: Połączenie dwóch tabel
-    echo("<h1>Zadanie 7: Połączenie dwóch tabel</h1>");
-    $result=$conn->query("SELECT id_pracownicy, imie, dzial, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE id_org=dzial");
-    echo("<table borded=1>");
-    echo("<th>id_pracownicy</th>");
-    echo("<th>imie</th>");
-    echo("<th>dzial</th>");
-    echo("<th>nazwa_dzial</th>");
-    echo("<th>zarobki</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 7: Połączenie dwóch tabel
+        echo("<h1>Zadanie 7: Połączenie dwóch tabel</h1>");
+        $result=$conn->query("SELECT id_pracownicy, imie, dzial, zarobki, data_urodzenia, nazwa_dzial FROM pracownicy, organizacja WHERE id_org=dzial");
+        echo("<table borded=1>");
+        echo("<th>id_pracownicy</th>");
+        echo("<th>imie</th>");
+        echo("<th>dzial</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>zarobki</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["zarobki"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
 
-    //Zadanie 8: Wyświetlenie średniej z połączonymi dwoma tabelami
-    echo("<h1>Zadanie 8: Wyświetlenie średniej zarobków z połączonymi dwoma tabelami</h1>");
-    $result=$conn->query("SELECT dzial, nazwa_dzial, AVG(zarobki) AS Średnia_zarobków FROM pracownicy, organizacja WHERE id_org=dzial GROUP BY dzial");
-    echo("<table borded=1>");
-    echo("<th>dzial</th>");
-    echo("<th>nazwa_dzial</th>");
-    echo("<th>Średnia_zarobków</th>");
-    while($row=$result->fetch_assoc())
-    {
-        echo("<tr>");
-        echo("<td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["Średnia_zarobków"]."</td>");
-        echo("</tr>");
-    }
-    echo("</table>");
+        //Zadanie 8: Wyświetlenie średniej z połączonymi dwoma tabelami
+        echo("<h1>Zadanie 8: Wyświetlenie średniej zarobków z połączonymi dwoma tabelami</h1>");
+        $result=$conn->query("SELECT dzial, nazwa_dzial, AVG(zarobki) AS Średnia_zarobków FROM pracownicy, organizacja WHERE id_org=dzial GROUP BY dzial");
+        echo("<table borded=1>");
+        echo("<th>dzial</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>Średnia_zarobków</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["Średnia_zarobków"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
+
+        //Zadanie 9: Wyświetlenie sumy z połączonymi dwoma tabelami
+        echo("<h1>Zadanie 9: Wyświetlenie sumy zarobków z połączonymi dwoma tabelami</h1>");
+        $result=$conn->query("SELECT dzial, nazwa_dzial, SUM(zarobki) AS Suma_zarobków FROM pracownicy, organizacja WHERE id_org=dzial GROUP BY dzial");
+        echo("<table borded=1>");
+        echo("<th>dzial</th>");
+        echo("<th>nazwa_dzial</th>");
+        echo("<th>Suma_zarobków</th>");
+        while($row=$result->fetch_assoc())
+        {
+            echo("<tr>");
+            echo("<td>".$row["dzial"]."</td><td>".$row["nazwa_dzial"]."</td><td>".$row["Suma_zarobków"]."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
     ?>
 </body>
 </html>
