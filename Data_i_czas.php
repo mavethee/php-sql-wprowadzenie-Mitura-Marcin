9d10175534e9babeecd259095e872d2ae49c2f1f
Marcin Mitura <mitura07@wp.pl>
2020-11-19 11:16:05 +0100

    Adding basic form and new linking in navs

--- Data_i_czas.php
+++ Data_i_czas.php
@@ -14,7 +14,10 @@
         <a href="Funkcje_agregujace.php">Funkcje_agregujące<a>;
         <a href="Data_i_czas.php">Data_i_czas<a>;
         <a href="Date_format.php">Formatowanie_dat<a>;
-        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub<a>;
+        <a href="formularz.html">Formularz<a>;
+        <a href="https://projekt-testowy-mavethee.herokuapp.com/">Testowanko<a>;
+        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub - PHP<a>;
+        <a href="https://github.com/mavethee/projekt_testowy">GitHub - Testowanko<a>;
     </div>
         <?php
 
--- Limit.php
+++ Limit.php
@@ -6,15 +6,18 @@
     </head>
 <body>
     <div class="nav">
-            Place for navigation and links
-            <a href="index.php">Główny index<a>;
-            <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy<a>;
-            <a href="Sortowanie.php">Sortowanie<a>;
-            <a href="Limit.php">Limit<a>;
-            <a href="Funkcje_agregujace.php">Funkcje_agregujące<a>;
-            <a href="Data_i_czas.php">Data_i_czas<a>;
-            <a href="Date_format.php">Formatowanie_dat<a>;
-            <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub<a>;
+        Place for navigation and links
+        <a href="index.php">Główny index<a>;
+        <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy<a>;
+        <a href="Sortowanie.php">Sortowanie<a>;
+        <a href="Limit.php">Limit<a>;
+        <a href="Funkcje_agregujace.php">Funkcje_agregujące<a>;
+        <a href="Data_i_czas.php">Data_i_czas<a>;
+        <a href="Date_format.php">Formatowanie_dat<a>;
+        <a href="formularz.html">Formularz<a>;
+        <a href="https://projekt-testowy-mavethee.herokuapp.com/">Testowanko<a>;
+        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub - PHP<a>;
+        <a href="https://github.com/mavethee/projekt_testowy">GitHub - Testowanko<a>;
     </div>
         <?php
 
