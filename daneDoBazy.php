cbfadedde0b3e5e5dd435ba2f0edca8cafbe497a
Marcin Mitura <mitura07@wp.pl>
2020-11-27 15:19:44 +0100

    Fixing nav links ...again? XD

--- daneDoBazy.php
+++ daneDoBazy.php
@@ -8,18 +8,18 @@
       </head>
 <body>
     <div class="nav">
-        <a href="index.php">Główny index<a>;
-        <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy<a>;
-        <a href="Sortowanie.php">Sortowanie<a>;
-        <a href="Limit.php">Limit<a>;
-        <a href="Funkcje_agregujace.php">Funkcje_agregujące<a>;
-        <a href="Data_i_czas.php">Data_i_czas<a>;
-        <a href="Date_format.php">Formatowanie_dat<a>;
-        <a href="formularz.html">Formularz- Strona.php<a>;
-        <a href="daneDoBazy.php">Formularz - daneDoBazy.php<a>;
-        <a href="https://projekt-testowy-mavethee.herokuapp.com/">Testowanko<a>;
-        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub - PHP<a>;
-        <a href="https://github.com/mavethee/projekt_testowy">GitHub - Testowanko<a>;
+        <a href="index.php">Główny index</a>
+        <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy</a>
+        <a href="Sortowanie.php">Sortowanie</a>
+        <a href="Limit.php">Limit</a>
+        <a href="Funkcje_agregujace.php">Funkcje_agregujące</a>
+        <a href="Data_i_czas.php">Data_i_czas</a>
+        <a href="Date_format.php">Formatowanie_dat</a>
+        <a href="formularz.html">Formularz - Strona.php</a>
+        <a href="daneDoBazy.php">Formularz - daneDoBazy.php</a>
+        <a href="https://projekt-testowy-mavethee.herokuapp.com/">Testowanko</a>
+        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub - PHP</a>
+        <a href="https://github.com/mavethee/projekt_testowy">GitHub - Testowanko</a>
     </div>
 
     <h2>Dodanie pracownika:</h2>
