f9d84666e199d1e47b25891cc023a54be32f3c67
Marcin Mitura <mitura07@wp.pl>
2020-11-27 16:50:46 +0100

    Updating debug.php

--- debug.php
+++ debug.php
@@ -1,9 +1,10 @@
 <?php
-//Debugging:
+//Debugging - put into following *.php you have issue with:
 echo "server<br/>";
 print_r($_SERVER);
 echo "post<br/>";
 print_r($_POST);
 echo "get<br/>";
 print_r($_GET);
+//Adding as a separate file just in case.
 ?>
