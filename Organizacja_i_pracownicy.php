4fe667ec5438acf5a8ee901a2370e3b215f181e6
Marcin Mitura <mitura07@wp.pl>
2020-12-01 20:51:10 +0100

    Assigning classes for a hrefs + styling (base for now test)

--- Organizacja_i_pracownicy.php
+++ Organizacja_i_pracownicy.php
@@ -8,19 +8,19 @@
     </head>
 <body>
     <div class="nav">
-        <a href="index.php">Główny index</a>
-        <a href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy</a>
-        <a href="Sortowanie.php">Sortowanie</a>
-        <a href="Limit.php">Limit</a>
-        <a href="Funkcje_agregujace.php">Funkcje_agregujące</a>
-        <a href="Data_i_czas.php">Data_i_czas</a>
-        <a href="Date_format.php">Formatowanie_dat</a>
-        <a href="formularz.html">Formularz - Strona.php</a>
-        <a href="daneDoBazy.php">Formularz - daneDoBazy.php</a>
-        <a href="Library.php">Książki</a>
-        <a href="https://projekt-testowy-mavethee.herokuapp.com/">Testowanko</a>
-        <a href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub - PHP</a>
-        <a href="https://github.com/mavethee/projekt_testowy">GitHub - Testowanko</a>
+        <a class="link a" href="index.php">Główny index</a>
+        <a class="link b" href="Organizacja_i_pracownicy.php">Organizacja_i_pracownicy</a>
+        <a class="link c" href="Sortowanie.php">Sortowanie</a>
+        <a class="link d" href="Limit.php">Limit</a>
+        <a class="link e" href="Funkcje_agregujace.php">Funkcje_agregujące</a>
+        <a class="link f" href="Data_i_czas.php">Data_i_czas</a>
+        <a class="link g" href="Date_format.php">Formatowanie_dat</a>
+        <a class="link h" href="formularz.html">Formularz - Strona.php</a>
+        <a class="link j" href="daneDoBazy.php">Formularz - daneDoBazy.php</a>
+        <a class="link k" href="Library.php">Książki</a>
+        <a class="link l" href="https://projekt-testowy-mavethee.herokuapp.com/">Testowanko</a>
+        <a class="link m" href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">GitHub - PHP</a>
+        <a class="link n" href="https://github.com/mavethee/projekt_testowy">GitHub - Testowanko</a>
     </div>
         <?php
             //Informacja o stronie:
