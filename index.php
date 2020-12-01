f6e21d397bdddde8053e95c5222f9143bf27a501
Marcin Mitura <mitura07@wp.pl>
2020-12-01 18:42:43 +0100

    Attempt to fix broken nav

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
             <?php
                 //Podpisanie pracy:
-                echo("<h1 class='Student_name'>Marcin Mitura</h1>");
+                echo("<h1 class='Student_name'><br>Marcin Mitura</br></h1>");
                 echo("<h1 class='Student_class_and_group'>Klasa 2Ti, grupa 1</h1>");
                 echo("<h1 class='Student_number'>Numer 22</h1>");
 
