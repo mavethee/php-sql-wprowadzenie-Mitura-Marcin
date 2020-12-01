db4c5d584e242206ab1800121bddc09d1ff77622
Marcin Mitura <mitura07@wp.pl>
2020-12-01 21:06:53 +0100

    Fixing missing classes and adding commands to CSS file

--- Library.php
+++ Library.php
@@ -25,7 +25,7 @@
             <?php
 
                 //Informacja o stronie:
-                echo("<h1><br>Jesteś w Library.php</h1></br>");
+                echo("<h1 class=page_info><br>Jesteś w Library.php</h1></br>");
 
                 //Logowanie do serwera mySQL:
                 require_once("connect.php");
