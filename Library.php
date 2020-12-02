125e243ea1afce98515979608ea5778998d39058
Marcin Mitura <mitura07@wp.pl>
2020-12-02 23:16:24 +0100

    Fixing nav links

--- Library.php
+++ Library.php
@@ -9,69 +9,69 @@
 <body>
     <div class="nav">
             <a class="link a" href="index.php">
-                <img class="nav_icon" src="nav_icon.png"/>
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg"/>
                     Główny index
-                <img class="nav_icon" src="nav_icon.png"/>
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg"/>
             </a>
             <a class="link b" href="Organizacja_i_pracownicy.php">
-                <img class="nav_icon" src="nav_icon.png"/>  
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg"/>  
                     Organizacja i pracownicy
-                <img class="nav_icon" src="nav_icon.png"/>
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg"/>
             </a>
             <a class="link c" href="Sortowanie.php">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Sortowanie
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link d" href="Limit.php">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Limit
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link e" href="Funkcje_agregujace.php">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Funkcje_agregujące
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link f" href="Data_i_czas.php">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Data_i_czas
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link g" href="Date_format.php">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Formatowanie_dat
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link h" href="formularz.html">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Formularz - Strona.php
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link j" href="daneDoBazy.php">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Formularz - daneDoBazy.php
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link k" href="Library.php">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Książki
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link l" href="https://projekt-testowy-mavethee.herokuapp.com/">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     Testowanko
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link m" href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     GitHub - PHP
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
             <a class="link n" href="https://github.com/mavethee/projekt_testowy">
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
                     GitHub - Testowanko
-                <img class="nav_icon" src="nav_icon.png">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/nav_icon.jpg">
             </a>
     </div>
             <?php
