ed560aaa0e4345aa77e3fab9288fed0395e01305
Marcin Mitura <mitura07@wp.pl>
2020-12-15 19:41:57 +0100

    Some important changes

---
+++ Formularze/delete.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width">
+            <title>Usuwanie pracownika</title>
+            <link rel="stylesheet" href="style.css">
+    </head>
+<body class="body_del">
+    <div class="nav">
+            <a class="link a" href="index.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png"/>
+                    Główny index
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png"/>
+            </a>
+            <a class="link b" href="Organizacja_i_pracownicy.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png"/>  
+                    Organizacja i pracownicy
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png"/>
+            </a>
+            <a class="link c" href="Sortowanie.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Sortowanie
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link d" href="Limit.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Limit
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link e" href="Funkcje_agregujace.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Funkcje agregujące
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link f" href="Data_i_czas.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Data i czas
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link g" href="Date_format.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Formatowanie dat
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link h" href="formularz.html">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Formularz - Strona.php
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link j" href="daneDoBazy.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Formularz - daneDoBazy.php
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link k" href="Library.php">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Książki
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link l" href="https://projekt-testowy-mavethee.herokuapp.com/">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    Testowanko
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link m" href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    GitHub - PHP
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+            <a class="link n" href="https://github.com/mavethee/projekt_testowy">
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+                    GitHub - Testowanko
+                <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
+            </a>
+    </div>
+
+                    <!--Skrypt pod autoplay muzyki w tle-->
+                    <script>
+                        window.onload = function() 
+                        {
+                        var context = new AudioContext();
+                        }
+                    </script>
+
+            <!--Muzyka w tle, zapętlona-->
+            <audio controls autoplay loop>
+                <source src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Muzyka/Kara_OST.mp3" type="audio/mpeg">
+            </audio>
+        <?php
+
+            //Informacja o stronie:
+            echo("<h4><br>Jesteś w delete.php</h1></br>");
+
+            //Wyświetlenie wysłanych danych przez użytkownika:           
+            echo("<h1 class=SQL_excercise> ID: ".$_POST["id_employee"]."</h1>");
+
+            //Logowanie do serwera mySQL:
+            require_once("connect.php");
+
+            //Zapytanie DELETE w $SQL z próbą uzyskania danych od użytkownika:
+            $sql = ("DELETE FROM pracownicy where id_pracownicy='".$_POST['id_employee']."'");
+            
+            //Wyświetlenie tabeli:
+            echo "<h1 class=SQL_excercise>". $sql;
+
+            //Informacja o sukcesie:
+            if ($conn->query($sql) === TRUE)
+            {
+                echo "<br>Record deleted successfully<br>";
+            }
+                        
+            //Informacja o ewentualnych błędach:
+            else 
+            {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            //Wyświetlenie tabelki jako podgląd przy usuwaniu pracowników:
+            $sql=("SELECT * FROM pracownicy");
+            echo("<h1 class=SQL_excercise>Wyświetlenie tabelki jako podgląd przy usuwaniu pracowników:</h1>");
+            echo("<h4 class=SQL_excercise>Użyte zapytanie SQL: ".$sql."</h4>");
+            $result=$conn->query("$sql");
+            echo("<table border=1>");
+            echo("<th>id_pracownicy</th>");
+            echo("<th>imie</th>");
+            echo("<th>data_urodzenia</th>");
+            echo("<th>dzial</th>");
+            echo("<th>zarobki</th>");
+            while($row=$result->fetch_assoc())
+            {
+                echo("<tr>");
+                echo("<td>".$row["id_pracownicy"]."</td><td>".$row["imie"]."</td><td>".$row["data_urodzenia"]."</td><td>".$row["dzial"]."</td><td>".$row["zarobki"]."</td>");
+                echo("</tr>");
+            }
+            echo("</table>");
+
+            $conn->close();
+        ?>
+</body>
+</html>
