ad17c1fba051bd8b220dbdc53e265bed37f2e719
Marcin Mitura <mitura07@wp.pl>
2020-12-17 11:18:43 +0100

    Fixing typos

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                 <div class="item colorRed">
                     <h1 class='Student_name'>Marcin Mitura</h1>
                     <h1 class='Student_class_and_group'>Klasa 2Ti, grupa 1</h1>
-                    <h1 class='Student_number'>Numer 22</h1>"
+                    <h1 class='Student_number'>Numer 22</h1>
                 </div>
                 <div class="item colorBlue">
                     <a class="link a" href="/index.php">
