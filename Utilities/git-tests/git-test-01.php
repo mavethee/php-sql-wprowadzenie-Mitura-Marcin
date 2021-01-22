8748edea4fd4458a9039c0803c793a0c0f930300
Marcin Mitura <mitura07@wp.pl>
2021-01-22 11:06:04 +0100

    git test commit

---
+++ Utilities/git-tests/git-test-01.php
@@ -0,0 +1,7 @@
+<?php
+echo("This file is only for weird ass homework. Why terminal when GUI exists? Terminal is for things what GUI can't easily reach. I will never understand people XD");
+echo("If you see this file, I feel sorry for you");
+
+# Push working :D
+
+?>
