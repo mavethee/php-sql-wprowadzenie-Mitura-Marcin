3ab3454daef818243ce2dab0d38d9de27a3991ff
Marcin Mitura <mitura07@wp.pl>
2021-01-26 10:27:47 +0100

    Updating Library site with flexbox formula

--- Biblioteka/Biblioteka.php
+++ Biblioteka/Biblioteka.php
@@ -7,7 +7,28 @@
         <link rel="stylesheet" href="/style.css">
     </head>
 <body class="body_lib">
-    <div class="nav">
+       <!--Skrypt pod autoplay muzyki w tle-->
+       <script>
+        window.onload = function() 
+        {
+            var context = new AudioContext();
+        }
+    </script>
+
+        <!--Muzyka w tle, zapętlona-->
+        <audio controls autoplay loop>
+            <source src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Muzyka/Kara_OST.mp3" type="audio/mpeg">
+        </audio>
+            <!--Tytuł strony w flexbox:-->
+            <div class="container">
+            <div class="item colorRed">
+                <?php
+                    //Informacja o stronie:
+                    echo("<h1 class=page_info><br>Jesteś w Library.php</h1></br>");
+                ?>
+            </div>
+            <!--Menu boczne w flexbox:-->
+            <div class="item colorBlue">
             <a class="link a" href="/index.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png"/>
                     Główny index
@@ -78,72 +99,56 @@
                     WESOŁYCH ŚWIĄT!
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Utilities/Ikony/nav_icon.png">
             </a>
-    </div>
+            </div>
+            <!--Treść strony w flexbox:-->
+            <div class="item colorGreen">
+                <?php
+                    //Logowanie do serwera mySQL:
+                    require_once($_SERVER['DOCUMENT_ROOT'] . '/connect.php');
 
-                    <!--Skrypt pod autoplay muzyki w tle-->
-                    <script>
-                        window.onload = function() 
-                        {
-                        var context = new AudioContext();
-                        }
-                    </script>
+                    //Zadanie 0: Funkcja wypożyczania:
+                    $sql1 = ("SELECT * FROM bibliotekaAutor"); //Autorzy
+                    $sql2 = ("SELECT * FROM bibliotekaTytuł"); //Książki
+                    echo("<h1 class=SQL_excercise>Zadanie 0: Funkcja wypożyczania</h1>");
+                    echo("<h4 class=SQL_excercise>Użyte zapytanie SQL do wyświetlenia autorów w formularzu: ".$sql1."</h4>");
+                    echo("<h4 class=SQL_excercise>Użyte zapytanie SQL do wyświetlenia tytułów w formularzu: ".$sql2."</h4>");
+                    $result=$conn->query($sql1);
+                    echo("<form action='wypozyczalnia.php' method='POST'>");
+                    echo("<label for='Autor'><h4 class='renting_form'>Wybierz autora:</h4></label>");
+                    echo("<select name='Autor' id='id_autor'>");
+                    while($row=$result->fetch_assoc()) 
+                    {
+                        echo("<option value=".$row['id_autor'].">".$row['Autor']."</option>");
+                    }
+                    echo("</select>");
+                    $result=$conn->query($sql2);
+                    echo("<label for='Tytuł'><h4 class='renting_form'>Wybierz tytuł:</h4></label>");
+                    echo("<select name='Tytuł' id='id_tytuł'>");
+                    while($row=$result->fetch_assoc())
+                    {
+                        echo("<option value=".$row['id_tytuł'].">".$row['Tytuł']."</option>");
+                    }
+                    echo("</select>");
+                    echo("<input type='submit' value='Wypożycz'>'");
+                    echo("</form>");
 
-            <!--Muzyka w tle, zapętlona-->
-            <audio controls autoplay loop>
-                <source src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/Kara_OST.mp3" type="audio/mpeg">
-            </audio>
-
-            <?php
-
-                //Informacja o stronie:
-                echo("<h1 class=page_info><br>Jesteś w Library.php</h1></br>");
-
-                //Logowanie do serwera mySQL:
-                require_once($_SERVER['DOCUMENT_ROOT'] . '/connect.php');
-
-                //Zadanie 0: Funkcja wypożyczania:
-                $sql1 = ("SELECT * FROM bibliotekaAutor"); //Autorzy
-                $sql2 = ("SELECT * FROM bibliotekaTytuł"); //Książki
-                echo("<h1 class=SQL_excercise>Zadanie 0: Funkcja wypożyczania</h1>");
-                echo("<h4 class=SQL_excercise>Użyte zapytanie SQL do wyświetlenia autorów w formularzu: ".$sql1."</h4>");
-                echo("<h4 class=SQL_excercise>Użyte zapytanie SQL do wyświetlenia tytułów w formularzu: ".$sql2."</h4>");
-                $result=$conn->query($sql1);
-                echo("<form action='wypozyczalnia.php' method='POST'>");
-                echo("<label for='Autor'><h4 class='renting_form'>Wybierz autora:</h4></label>");
-                echo("<select name='Autor' id='id_autor'>");
-                while($row=$result->fetch_assoc()) 
-                {
-                    echo("<option value=".$row['id_autor'].">".$row['Autor']."</option>");
-                }
-                echo("</select>");
-                $result=$conn->query($sql2);
-                echo("<label for='Tytuł'><h4 class='renting_form'>Wybierz tytuł:</h4></label>");
-                echo("<select name='Tytuł' id='id_tytuł'>");
-                while($row=$result->fetch_assoc())
-                {
-                    echo("<option value=".$row['id_tytuł'].">".$row['Tytuł']."</option>");
-                }
-                echo("</select>");
-                echo("<input type='submit' value='Wypożycz'>'");
-                echo("</form>");
-
-                //Zadanie 1: Wyświetlenie pełnej bazy danych ksiąg w blibiotece:
-                $sql = ('SELECT * FROM bibliotekaAT, bibliotekaAutor, bibliotekaTytuł WHERE id_autor=bibliotekaAutor_ID AND id_tytuł=bibliotekaTytul_ID');
-                echo("<h1 class=SQL_excercise>Zadanie 1: Wyświetlenie pełnej bazy danych w blibiotece</h1>");
-                echo("<h4 class=SQL_excercise>Użyte zapytanie SQL: ".$sql."</h4>");
-                $result=$conn->query($sql);
-                echo("<table border=1>");
-                echo("<th>id</th>");
-                echo("<th>Autor</th>");
-                echo("<th>Tytuł</th>");
-                while($row=$result->fetch_assoc())
-                {
-                    echo("<tr>");
-                    echo("<td>".$row["id"]."</td><td>".$row["Autor"]."</td><td>".$row["Tytuł"]);
-                    echo("</tr>");
-                }
-                echo("</table>");
-
-            ?>
-        </body>
+                    //Zadanie 1: Wyświetlenie pełnej bazy danych ksiąg w blibiotece:
+                    $sql = ('SELECT * FROM bibliotekaAT, bibliotekaAutor, bibliotekaTytuł WHERE id_autor=bibliotekaAutor_ID AND id_tytuł=bibliotekaTytul_ID');
+                    echo("<h1 class=SQL_excercise>Zadanie 1: Wyświetlenie pełnej bazy danych w blibiotece</h1>");
+                    echo("<h4 class=SQL_excercise>Użyte zapytanie SQL: ".$sql."</h4>");
+                    $result=$conn->query($sql);
+                    echo("<table border=1>");
+                    echo("<th>id</th>");
+                    echo("<th>Autor</th>");
+                    echo("<th>Tytuł</th>");
+                    while($row=$result->fetch_assoc())
+                    {
+                        echo("<tr>");
+                        echo("<td>".$row["id"]."</td><td>".$row["Autor"]."</td><td>".$row["Tytuł"]);
+                        echo("</tr>");
+                    }
+                    echo("</table>");
+                ?>
+            </div>
+</body>
 </html>
