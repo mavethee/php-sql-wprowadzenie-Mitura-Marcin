0e271eb428abed1c28955287e2749e6ff90c3f97
Marcin Mitura <mitura07@wp.pl>
2021-02-16 11:18:08 +0100

    Fixing favicons displayment

--- Organizacja i pracownicy/Funkcje_agregujace.php
+++ Organizacja i pracownicy/Funkcje_agregujace.php
@@ -4,23 +4,23 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <title>Funkcje agregujące</title>
-        <link rel="stylesheet" href=
-        <link rel="apple-touch-icon" sizes="57x57" href=con-57x57.png'">
-        <link rel="apple-touch-icon" sizes="60x60" href=con-60x60.png'">
-        <link rel="apple-touch-icon" sizes="72x72" href=con-72x72.png'">
-        <link rel="apple-touch-icon" sizes="76x76" href=con-76x76.png'">
-        <link rel="apple-touch-icon" sizes="114x114" href=con-114x114.png'">
-        <link rel="apple-touch-icon" sizes="120x120" href=con-120x120.png'">
-        <link rel="apple-touch-icon" sizes="144x144" href=con-144x144.png'">
-        <link rel="apple-touch-icon" sizes="152x152" href=con-152x152.png'">
-        <link rel="apple-touch-icon" sizes="180x180" href=con-180x180.png'">
-        <link rel="icon" type="image/png" sizes="192x192"  href=-icon-192x192.png'">
-        <link rel="icon" type="image/png" sizes="32x32" href=-32x32.png'">
-        <link rel="icon" type="image/png" sizes="96x96" href=-96x96.png'">
-        <link rel="icon" type="image/png" sizes="16x16" href=-16x16.png'">
-        <link rel="manifest" href=t.json'">
+        <link rel="stylesheet" href="/assets/style.css">
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/assets/icons/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/assets/icons/favicons/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content=-144x144.png'">
+        <meta name="msapplication-TileImage" content="/assets/icons/favicons/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
     </head>
 <body class="body_aggrefunc">
