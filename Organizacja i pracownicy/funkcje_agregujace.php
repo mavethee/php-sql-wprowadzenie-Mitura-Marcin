516a54a09bacefd4345ff84b332d1a7350acd4af
mavethee <mitura07@wp.pl>
2021-02-16 20:40:50 +0100

    Fixing duplicated nav

--- Organizacja i pracownicy/funkcje_agregujace.php
+++ Organizacja i pracownicy/funkcje_agregujace.php
@@ -34,7 +34,6 @@
         <!--Menu boczne w flexbox:-->
         <div class="item colorBlue">
             <a class="link a" href="/index.php">
-            <a class="link a" href="/index.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     Główny index
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
