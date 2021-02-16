60b72bdb2060650b734d5ae0b3f2a57816b20753
Marcin Mitura <mitura07@wp.pl>
2021-02-16 08:33:49 +0100

    Testing favicons

--- index.php
+++ index.php
@@ -5,6 +5,23 @@
         <meta name="viewport" content="width=device-width">
         <title>Główny Index</title>
         <link rel="stylesheet" href="/style.css">
+        <link rel="apple-touch-icon" sizes="57x57" href="/Utilities/Ikony/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/Utilities/Ikony/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/Utilities/Ikony/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/Utilities/Ikony/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/Utilities/Ikony/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/Utilities/Ikony/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/Utilities/Ikony/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/Utilities/Ikony/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/Utilities/Ikony/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/Utilities/Ikony/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/Utilities/Ikony/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/Utilities/Ikony/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/Utilities/Ikony/favicon-16x16.png">
+        <link rel="manifest" href="/Utilities/Ikony/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/Utilities/Ikony/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
     </head>
 <body>
 
