3213df5c5a94afa18d93a627e9a63d8af3fbca7f
Marcin Mitura <mitura07@wp.pl>
2021-02-16 10:55:20 +0100

    More and more fixes

--- Data i czas/Formatowanie_dat.php
+++ Data i czas/Formatowanie_dat.php
@@ -4,23 +4,23 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width">
         <title>Formatowanie dat</title>
-        <link rel="stylesheet" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/style.css'">
-        <link rel="apple-touch-icon" sizes="57x57" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-57x57.png'">
-        <link rel="apple-touch-icon" sizes="60x60" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-60x60.png'">
-        <link rel="apple-touch-icon" sizes="72x72" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-72x72.png'">
-        <link rel="apple-touch-icon" sizes="76x76" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-76x76.png'">
-        <link rel="apple-touch-icon" sizes="114x114" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-114x114.png'">
-        <link rel="apple-touch-icon" sizes="120x120" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-120x120.png'">
-        <link rel="apple-touch-icon" sizes="144x144" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-144x144.png'">
-        <link rel="apple-touch-icon" sizes="152x152" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-152x152.png'">
-        <link rel="apple-touch-icon" sizes="180x180" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/apple-icon-180x180.png'">
-        <link rel="icon" type="image/png" sizes="192x192"  href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/android-icon-192x192.png'">
-        <link rel="icon" type="image/png" sizes="32x32" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/favicon-32x32.png'">
-        <link rel="icon" type="image/png" sizes="96x96" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/favicon-96x96.png'">
-        <link rel="icon" type="image/png" sizes="16x16" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/favicon-16x16.png'">
-        <link rel="manifest" href="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/manifest.json'">
+        <link rel="stylesheet" href="/assets/style.css">
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/favicons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/favicons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/favicons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/favicons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/favicons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/favicons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/favicons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/favicons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/favicons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/assets/icons/favicons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/assets/icons/favicons/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="$_SERVER['DOCUMENT_ROOT'] . '/assets/icons/favicons/ms-icon-144x144.png'">
+        <meta name="msapplication-TileImage" content="/assets/icons/favicons/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
     </head>
 <body class="body_df">
@@ -46,52 +46,52 @@
             </div>
         <!--Menu boczne w flexbox:-->
         <div class="item colorBlue">
-            <a class="link a" href="$_SERVER['DOCUMENT_ROOT'] . '/index.php'">
+            <a class="link a" href=/index.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     Główny index
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
             </a>
-            <a class="link b" href="$_SERVER['DOCUMENT_ROOT'] . '/Organizacja%20i%20pracownicy/organizacja_i_pracownicy.php'">
+            <a class="link b" href=/Organizacja%20i%20pracownicy/organizacja_i_pracownicy.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>  
                     Organizacja i pracownicy
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
             </a>
-            <a class="link c" href="$_SERVER['DOCUMENT_ROOT'] . '/Organizacja%20i%20pracownicy/sortowanie.php'">sortowanie.php'">
+            <a class="link c" href=/Organizacja%20i%20pracownicy/sortowanie.php'">sortowanie.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Sortowanie
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link d" href="$_SERVER['DOCUMENT_ROOT'] . '/limit.php'">
+            <a class="link d" href=/limit.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Limit
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link e" href="$_SERVER['DOCUMENT_ROOT'] . '/Organizacja%20i%20pracownicy/funkcje_agregujace.php'">
+            <a class="link e" href=/Organizacja%20i%20pracownicy/funkcje_agregujace.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Funkcje agregujące
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link f" href="$_SERVER['DOCUMENT_ROOT'] . '/Data%20i%20czas/data_i_czas.php'">
+            <a class="link f" href=/Data%20i%20czas/data_i_czas.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Data i czas
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link g" href="$_SERVER['DOCUMENT_ROOT'] . '/Data%20i%20czas/formatowanie_dat.php'">
+            <a class="link g" href=/Data%20i%20czas/formatowanie_dat.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Formatowanie dat
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link h" href="$_SERVER['DOCUMENT_ROOT'] . '/Formularze/formularz.html'"">
+            <a class="link h" href=/Formularze/formularz.html'"">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Formularz - Strona.php
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link j" href="$_SERVER['DOCUMENT_ROOT'] . '/Formularze/daneDoBazy.php'">
+            <a class="link j" href=/Formularze/daneDoBazy.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Formularz - daneDoBazy.php
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link k" href="$_SERVER['DOCUMENT_ROOT'] . '/Biblioteka/biblioteka.php'">
+            <a class="link k" href=/Biblioteka/biblioteka.php'">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Biblioteka
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
