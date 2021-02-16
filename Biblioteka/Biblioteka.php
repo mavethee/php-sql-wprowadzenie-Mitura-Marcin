2d8b68698e285492b0ca5d611abea8bf4cb777cf
Marcin Mitura <mitura07@wp.pl>
2021-02-16 11:03:39 +0100

    Fixing nav and favicons in library

--- Biblioteka/Biblioteka.php
+++ Biblioteka/Biblioteka.php
@@ -56,42 +56,42 @@
                     Organizacja i pracownicy
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
             </a>
-            <a class="link c" href=/_SERVER['DOCUMENT_ROOT'] . '/Organizacja%20i%20pracownicy/sortowanie.php'">>
+            <a class="link c" href="/Organizacja%20i%20pracownicy/sortowanie.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Sortowanie
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link d" href=/_SERVER['DOCUMENT_ROOT'] . '/limit.php
+            <a class="link d" href="/Organizacja%20i%20pracownicy/limit.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Limit
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link e" href=/_SERVER['DOCUMENT_ROOT'] . '/Organizacja%20i%20pracownicy/funkcje_agregujace.php'">>
+            <a class="link e" href="/Organizacja%20i%20pracownicy/funkcje_agregujace.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Funkcje agregujące
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link f" href=/_SERVER['DOCUMENT_ROOT'] . '/Data%20i%20czas/data_i_czas.php'">>
+            <a class="link f" href="/Data%20i%20czas/data_i_czas.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Data i czas
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link g" href=/_SERVER['DOCUMENT_ROOT'] . '/Data%20i%20czas/formatowanie_dat.php'">>
+            <a class="link g" href="/Data%20i%20czas/formatowanie_dat.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Formatowanie dat
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link h" href=/_SERVER['DOCUMENT_ROOT'] . '/Formularze/formularz.html'"'"">
+            <a class="link h" href="/Formularze/formularz.html">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Formularz - Strona.php
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link j" href=/_SERVER['DOCUMENT_ROOT'] . '/Formularze/daneDoBazy.php'">>
+            <a class="link j" href="/Formularze/daneDoBazy.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Formularz - daneDoBazy.php
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
             </a>
-            <a class="link k" href=/_SERVER['DOCUMENT_ROOT'] . '/Biblioteka/biblioteka.php'">>
+            <a class="link k" href="/Biblioteka/biblioteka.php">
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
                     Biblioteka
                 <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/nav_icon.png">
@@ -122,7 +122,7 @@
                     echo("<h4 class=SQL_excercise>Użyte zapytanie SQL do wyświetlenia autorów w formularzu: ".$sql1."</h4>");
                     echo("<h4 class=SQL_excercise>Użyte zapytanie SQL do wyświetlenia tytułów w formularzu: ".$sql2."</h4>");
                     $result=$conn->query($sql1);
-                    echo("<form action='Wypozyczalnia.php' method='POST'>");
+                    echo("<form action='wypozyczalnia.php' method='POST'>");
                     echo("<label for='Autor'><h4 class='renting_form'>Wybierz autora:</h4></label>");
                     echo("<select name='Autor' id='id_autor'>");
                     while($row=$result->fetch_assoc()) 
