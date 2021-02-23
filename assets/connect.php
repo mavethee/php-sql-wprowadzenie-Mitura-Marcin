0a5b18fb989a87b6f41b4cd7b57677eec2c5b521
mavethee <mitura07@wp.pl>
2021-02-23 11:00:16 +0100

    Fixing connect

--- assets/connect.php
+++ assets/connect.php
@@ -1,4 +1,9 @@
 <?php
-//Connection
+//Create connection
 $conn = new mysqli($_SERVER['SERVER'],$_SERVER['USER'], $_SERVER['PASS'], $_SERVER['DBNAME']);
+// Check connection
+if ($conn->connect_error) 
+{
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
