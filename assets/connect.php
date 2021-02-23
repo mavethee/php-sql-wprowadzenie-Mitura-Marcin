108c9329da58b91e884429059bd5949b327320a7
mavethee <mitura07@wp.pl>
2021-02-23 10:03:31 +0100

    Work pls OwO

--- assets/connect.php
+++ assets/connect.php
@@ -1,9 +1,4 @@
 <?php
-// Create connection
-$conn = new mysqli($_SERVER['SERVER'],$_SERVER['USER'], $_SERVER['PASS'], $_SERVER['DBNAME']);
-// Check connection
-if ($conn->connect_error) 
-{
-  die("Connection failed: " . $conn->connect_error);
-}
+//Connection
+$conn = new mysqli($_SERVER['SERVER'], $_SERVER['USER'], $_SERVER['PASS'], $_SERVER['DBNAME']);
 ?>
