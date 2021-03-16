8a62d1f7a6e473da9c76ee531b29aa789dca3b60
mavethee <mitura07@wp.pl>
2021-03-16 10:57:27 +0100

    change

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                 <div class="item colorRed">
                     <h1 class='Student_name'>Marcin Mitura</h1>
                     <h1 class='Student_class_and_group'>Klasa 2Ti, grupa 1</h1>
-                    <h1 class='Student_number'>Numer 22</h1>
+                    <h1 class='Student_number'>Numer 212</h1>
                 </div>
                 <!--Menu boczne w flexbox:-->
                 <div class="item colorBlue">
