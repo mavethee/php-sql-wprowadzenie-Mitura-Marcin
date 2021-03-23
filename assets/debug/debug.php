bd8475dc5fecdc04b92aa0b31d258ef8eccd6820
mavethee <mitura07@wp.pl>
2021-03-23 09:35:18 +0100

    Debugging some info

--- assets/debug/debug.php
+++ assets/debug/debug.php
@@ -1,11 +1,8 @@
 <?php
 //Debugging - put into following *.php you have issue with:
-echo "server<br/>";
-print_r($_SERVER);
-echo "post<br/>";
-print_r($_POST);
-echo "get<br/>";
-print_r($_GET);
+print($_SERVER['MYSQL_SERVER_LOCAL']);
+print($_SERVER['MYSQL_USER_LOCAL']);
+print($_SERVER['MYSQL_DBNAME_LOCAL']);
 //Adding as a separate file just in case.
 //I need to use this file more.
 ?>
