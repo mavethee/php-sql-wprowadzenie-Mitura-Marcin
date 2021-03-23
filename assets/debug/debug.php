340753d11506ce17f1a07e05cbe685ae6f66c454
mavethee <mitura07@wp.pl>
2021-03-23 09:39:19 +0100

    debug

--- assets/debug/debug.php
+++ assets/debug/debug.php
@@ -2,6 +2,7 @@
 //Debugging - put into following *.php you have issue with:
 print($_SERVER['MYSQL_SERVER']);
 print($_SERVER['MYSQL_USER']);
+print($_SERVER['MYSQL_PASS']);
 print($_SERVER['MYSQL_DBNAME']);
 //Adding as a separate file just in case.
 //I need to use this file more.
