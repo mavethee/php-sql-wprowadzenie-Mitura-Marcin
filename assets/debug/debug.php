89639834d83deaa243d72b2338a76095c933289f
mavethee <mitura07@wp.pl>
2021-03-23 10:04:40 +0100

    Removing debug info

--- assets/debug/debug.php
+++ assets/debug/debug.php
@@ -1,9 +1,9 @@
 <?php
 //Debugging - put into following *.php you have issue with:
-print($_SERVER['MYSQL_SERVER']);
-print($_SERVER['MYSQL_USER']);
-print($_SERVER['MYSQL_PASS']);
-print($_SERVER['MYSQL_DBNAME']);
+//print($_SERVER['MYSQL_SERVER']);
+//print($_SERVER['MYSQL_USER']);
+//print($_SERVER['MYSQL_PASS']);
+//print($_SERVER['MYSQL_DBNAME']);
 //Adding as a separate file just in case.
 //I need to use this file more.
 ?>
