3f943c0e8b58e72bd4429716c7f809c8f17fa273
mavethee <mitura07@wp.pl>
2021-03-23 09:05:14 +0100

    Correcting code formatting in connect

--- assets/connect.php
+++ assets/connect.php
@@ -1,9 +1,13 @@
 <?php
-//Create connection:
-$conn = new mysqli($_SERVER['MYSQL_SERVER'],$_SERVER['MYSQL_USER'], $_SERVER['MYSQL_PASS'], $_SERVER['MYSQL_DBNAME']);
-// Check connection:
-if ($conn->connect_error) 
-{
-  die("Connection failed: " . $conn->connect_error);
-}
+  //Create connection:
+    $conn = new mysqli($_SERVER['MYSQL_SERVER'],$_SERVER['MYSQL_USER'], $_SERVER['MYSQL_PASS'], $_SERVER['MYSQL_DBNAME']);
+      //Check connection:
+        if ($conn->connect_error) 
+        {
+          die("Connection failed: " . $conn->connect_error);
+        }
+          else
+          {
+            echo("Connected successfully");
+          }
 ?>
