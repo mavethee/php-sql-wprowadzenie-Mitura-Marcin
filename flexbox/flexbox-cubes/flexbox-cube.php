f16c48e8f8bef0b8605185abcaddda1197ebefa8
mavethee <mitura07@wp.pl>
2021-04-13 11:45:26 +0200

    Styling Flexbox Cube

--- flexbox/flexbox-cubes/flexbox-cube.php
+++ flexbox/flexbox-cubes/flexbox-cube.php
@@ -34,72 +34,72 @@
         </div>
                 <!--Menu boczne w flexbox:-->
                 <div class="item colorBlue">
-                    <a class="link a" href="/index.php">
+                    <a class="nav_link a" href="/index.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Główny index
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link b" href="/Organizacja%20i%20pracownicy/organizacja_i_pracownicy.php">
+                    <a class="nav_link b" href="/Organizacja%20i%20pracownicy/organizacja_i_pracownicy.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>  
                             Organizacja i pracownicy
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link c" href="/Organizacja%20i%20pracownicy/sortowanie.php">
+                    <a class="nav_link c" href="/Organizacja%20i%20pracownicy/sortowanie.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Sortowanie
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link d" href="/Organizacja%20i%20pracownicy/limit.php">
+                    <a class="nav_link d" href="/Organizacja%20i%20pracownicy/limit.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Limit
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link e" href="/Organizacja%20i%20pracownicy/funkcje_agregujace.php">
+                    <a class="nav_link e" href="/Organizacja%20i%20pracownicy/funkcje_agregujace.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Funkcje agregujące
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link f" href="/Data%20i%20czas/data_i_czas.php">
+                    <a class="nav_link f" href="/Data%20i%20czas/data_i_czas.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Data i czas
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link g" href="/Data%20i%20czas/formatowanie_dat.php">
+                    <a class="nav_link g" href="/Data%20i%20czas/formatowanie_dat.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                               Formatowanie dat
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link h" href="/Formularze/formularz.html">
+                    <a class="nav_link h" href="/Formularze/formularz.html">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Formularz - Strona.php
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link j" href="/Formularze/daneDoBazy.php">
+                    <a class="nav_link j" href="/Formularze/daneDoBazy.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Formularz - daneDoBazy.php
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link k" href="/Biblioteka/biblioteka.php">
+                    <a class="nav_link k" href="/Biblioteka/biblioteka.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Biblioteka
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link l" href="/flexbox/flexbox-cubes/flexbox-cube.php">
+                    <a class="nav_link l" href="/flexbox/flexbox-cubes/flexbox-cube.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Flexbox
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link m" href="https://projekt-testowy-mavethee.herokuapp.com/">
+                    <a class="nav_link m" href="https://projekt-testowy-mavethee.herokuapp.com/">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Testowanko
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link n" href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">
+                    <a class="nav_link n" href="https://github.com/SK-2019/php-sql-wprowadzenie-Mitura-Marcin">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             GitHub - PHP
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                     </a>
-                    <a class="link o" href="https://github.com/mavethee/projekt_testowy">
+                    <a class="nav_link o" href="https://github.com/mavethee/projekt_testowy">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             GitHub - Testowanko
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
@@ -123,92 +123,92 @@
                 <div class="item colorGreen">
                     <h1>Zadanie 1</h1>
                     <div class="container">
-                        <div class="nav">
-                            <a class="link" href="https://www.rpcs3.net/">LINK</a>
-                            <a class="link" href="https://www.rpcs3.net/">LINK</a>
-                            <a class="link" href="https://www.rpcs3.net/">LINK</a>
+                        <div class="nav-ex">
+                            <a class="nav_link" href="https://www.rpcs3.net/">nav_link</a>
+                            <a class="nav_link" href="https://www.rpcs3.net/">nav_link</a>
+                            <a class="nav_link" href="https://www.rpcs3.net/">nav_link</a>
                         </div>
                     </div>
                     <h1>Zadanie 2</h1>
                     <div class="container2">
-                        <div class="nav">
-                            <a class="link" href="https://www.pcsx2.net/">LINK</a>
-                            <a class="link" href="https://www.pcsx2.net/">LINK</a>
-                            <a class="link" href="https://www.pcsx2.net/">LINK</a>
+                        <div class="nav-ex">
+                            <a class="nav_link" href="https://www.pcsx2.net/">nav_link</a>
+                            <a class="nav_link" href="https://www.pcsx2.net/">nav_link</a>
+                            <a class="nav_link" href="https://www.pcsx2.net/">nav_link</a>
                         </div>
                     </div>
                     <h1>Zadanie 3</h1>
                     <div class="container3">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 3
                         </div>
                     </div>
                     <h1>Zadanie 4</h1>
                     <div class="container4">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 4
                         </div>
                     </div>
                     <h1>Zadanie 5</h1>
                     <div class="container5">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 5
                         </div>
                     </div>
                     <h1>Zadanie 6</h1>
                     <div class="container6">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 6
                         </div>
                     </div>
                     <h1>Zadanie 7</h1>
                     <div class="container7">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 7
                         </div>
                     </div>
                     <h1>Zadanie 8</h1>
                     <div class="container8">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 8
                         </div>
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 8
                         </div>
                     </div>
                     <h1>Zadanie 9</h1>
                     <div class="container9">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 9
                         </div>
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 9
                         </div>
                     </div>
                     <h1>Zadanie 10</h1>
                     <div class="container10">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 10
                         </div>
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 10
                         </div>
                     </div>
                     <h1>Zadanie 11</h1>
                     <div class="container11">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 11
                         </div>
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 11
                         </div>
                     </div>
                     <h1>Zadanie 12</h1>
                     <div class="container12">
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 12
                         </div>
-                        <div class="nav">
+                        <div class="nav-ex">
                             zad 12
                         </div>
                 </div>
