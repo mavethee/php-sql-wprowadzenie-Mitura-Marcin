5d066f28e2723dbd41654b962063dc7d25218a2f
mavethee <mitura07@wp.pl>
2021-04-20 10:05:21 +0200

    Preparing headers in separated files.

---
+++ assets/headers/header_index.php
@@ -0,0 +1,3 @@
+<h1 class='Student_name'>Marcin Mitura</h1>
+<h1 class='Student_class_and_group'>Klasa 2Ti, grupa 1</h1>
+<h1 class='Student_number'>Numer 22</h1>
--- index.php
+++ index.php
@@ -40,9 +40,7 @@
             <!--Podpisanie pracy w flexbox:-->
             <div class="container">
                 <div class="item colorRed">
-                    <h1 class='Student_name'>Marcin Mitura</h1>
-                    <h1 class='Student_class_and_group'>Klasa 2Ti, grupa 1</h1>
-                    <h1 class='Student_number'>Numer 22</h1>
+                    <?php include("assets/headers/header_index.php") ?>
                 </div>
                 <!--Menu boczne w flexbox:-->
                 <div class="item colorBlue">
