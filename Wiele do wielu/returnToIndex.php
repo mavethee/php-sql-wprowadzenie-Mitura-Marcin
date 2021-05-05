3af57c8e52975aac04a4e4f8846e2099d8506b77
mavethee <mitura07@wp.pl>
2021-05-05 21:21:28 +0200

    typo again lol

--- Wiele do wielu/returnToIndex.php
+++ Wiele do wielu/returnToIndex.php
@@ -1,4 +1,4 @@
-                    <a class="link a" href="library.php">
+                    <a class="link a" href="index.php">
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
                             Relacje wiele do wielu - strona główna
                         <img class="nav_icon" src="https://raw.githubusercontent.com/sk-2019/php-sql-wprowadzenie-mitura-marcin/main/assets/icons/navicons/nav_icon.png"/>
